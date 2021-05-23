215220cce22f8887b94afe802d160d98055549aa
Patrick Boyd <ppatboyd@outlook.com>
2021-05-22 17:28:55 -0700

    Attempt to enable nightbot command access to built NightBotChannelsModded

--- NightBotChannelsModded.ts
+++ NightBotChannelsModded.ts
@@ -2,43 +2,45 @@
 import { Channel, Moderator } from './NightBotModCheck';
   
   
-  const wooordsMod : Moderator = {
-    status:200,
-    user:"woooords",
-    cursor:"",
-    channels:[
-      {
-        name:"akanemsko",
-        followers:261120,
-        views:23508664,
-        partnered:true
-      },
-      {
-        name:"chessweeb",
-        followers:16627,
-        views:3338792,
-        partnered:true
-      },
-      {
-        name:"tfblade",
-        followers:1116175,
-        views:83458655,
-        partnered:true
-      },
-      {
-        name:"woohoot",
-        followers:77,
-        views:1447,
-        partnered:false
-      }
-     ]};
+//   const mod : Moderator = {
+//     status:200,
+//     user:"woooords",
+//     cursor:"",
+//     channels:[
+//       {
+//         name:"akanemsko",
+//         followers:261120,
+//         views:23508664,
+//         partnered:true
+//       },
+//       {
+//         name:"chessweeb",
+//         followers:16627,
+//         views:3338792,
+//         partnered:true
+//       },
+//       {
+//         name:"tfblade",
+//         followers:1116175,
+//         views:83458655,
+//         partnered:true
+//       },
+//       {
+//         name:"woohoot",
+//         followers:77,
+//         views:1447,
+//         partnered:false
+//       }
+//      ]};
 
-  let channelsModerated : string = "Wooords moderates for: ";
+let mod : Moderator;
 
-  wooordsMod.channels.forEach((channel:Channel) =>{
+  let channelsModerated : string = mod.user + " moderates for: ";
+
+  mod.channels.forEach((channel:Channel) =>{
     channelsModerated = channelsModerated + channel.name + ", ";
   });
 
   channelsModerated = channelsModerated.substring(0, channelsModerated.length-2);
 
-  console.log(channelsModerated);
+  channelsModerated;
--- dist/NightBotChannelsModded.js
+++ dist/NightBotChannelsModded.js
@@ -1,40 +1,40 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var wooordsMod = {
-    status: 200,
-    user: "woooords",
-    cursor: "",
-    channels: [
-        {
-            name: "akanemsko",
-            followers: 261120,
-            views: 23508664,
-            partnered: true
-        },
-        {
-            name: "chessweeb",
-            followers: 16627,
-            views: 3338792,
-            partnered: true
-        },
-        {
-            name: "tfblade",
-            followers: 1116175,
-            views: 83458655,
-            partnered: true
-        },
-        {
-            name: "woohoot",
-            followers: 77,
-            views: 1447,
-            partnered: false
-        }
-    ]
-};
-var channelsModerated = "Wooords moderates for: ";
-wooordsMod.channels.forEach(function (channel) {
+//   const mod : Moderator = {
+//     status:200,
+//     user:"woooords",
+//     cursor:"",
+//     channels:[
+//       {
+//         name:"akanemsko",
+//         followers:261120,
+//         views:23508664,
+//         partnered:true
+//       },
+//       {
+//         name:"chessweeb",
+//         followers:16627,
+//         views:3338792,
+//         partnered:true
+//       },
+//       {
+//         name:"tfblade",
+//         followers:1116175,
+//         views:83458655,
+//         partnered:true
+//       },
+//       {
+//         name:"woohoot",
+//         followers:77,
+//         views:1447,
+//         partnered:false
+//       }
+//      ]};
+var mod;
+var channelsModerated = mod.user + " moderates for: ";
+mod.channels.forEach(function (channel) {
     channelsModerated = channelsModerated + channel.name + ", ";
 });
 channelsModerated = channelsModerated.substring(0, channelsModerated.length - 2);
-console.log(channelsModerated);
+channelsModerated;
 //# sourceMappingURL=NightBotChannelsModded.js.map
