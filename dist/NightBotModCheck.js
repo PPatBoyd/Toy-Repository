cb435297d385314966559af2d2fb22cd0211dcf1
Patrick Boyd <ppatboyd@outlook.com>
2021-05-22 17:23:03 -0700

    Update dist

---
+++ dist/NightBotModCheck.js
@@ -0,0 +1,43 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.FilterModerators = exports.FilterChannels = exports.filterProps = void 0;
+var lodash_1 = require("lodash");
+function filterProps(channel, allowedProps) {
+    return Object.fromEntries(Object.entries(channel).filter(function (_a) {
+        var key = _a[0] /*, value*/;
+        return (allowedProps.includes(key));
+    }));
+}
+exports.filterProps = filterProps;
+// Returns a filtered copy of a channel list
+function FilterChannels(channels, filterOptions, channelMatch, channelPropertyFilter) {
+    // Apply filter options
+    if (filterOptions.mutation == "copy") {
+        channels = lodash_1.cloneDeep(channels);
+    }
+    // Filter down to channels that match our spec
+    var filteredChannels = channels.filter(lodash_1.matches(channelMatch));
+    // Iterate over the channels and filter down to the channel properties we want
+    filteredChannels.forEach(function (channel, index, array) {
+        array[index] = filterProps(channel, channelPropertyFilter);
+    });
+    return filteredChannels;
+}
+exports.FilterChannels = FilterChannels;
+// Returns a filtered copy of a moderator list
+function FilterModerators(mods, filterOptions, modMatch, modPropFilter, channelMatch, channelPropFilter) {
+    // Apply filter options
+    if (filterOptions.mutation == "copy") {
+        mods = lodash_1.cloneDeep(mods);
+    }
+    var filteredMods = mods.filter(lodash_1.matches(modMatch));
+    filteredMods.forEach(function (mod, index, array) {
+        // Filter each mod's channels
+        mod.channels = FilterChannels(mod.channels, filterOptions, channelMatch, channelPropFilter);
+        // Filter down to the mod properties we want
+        array[index] = filterProps(mod, modPropFilter);
+    });
+    return filteredMods;
+}
+exports.FilterModerators = FilterModerators;
+//# sourceMappingURL=NightBotModCheck.js.map
--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var lodash_1 = require("lodash");
+var NightBotModCheck_1 = require("./NightBotModCheck");
 var SoManyMods = [{
         status: 200,
         user: "woooords",
@@ -45,53 +45,11 @@ var SoManyMods = [{
             }
         ]
     }];
-function filterProps(channel, allowedProps) {
-    return Object.fromEntries(Object.entries(channel).filter(function (_a) {
-        var key = _a[0] /*, value*/;
-        return (allowedProps.includes(key));
-    }));
-}
-// Returns a filtered copy of a channel list
-function FilterChannels(channels, filterOptions, channelMatch, channelPropertyFilter) {
-    // Apply filter options
-    if (filterOptions.mutation == "copy") {
-        channels = lodash_1.cloneDeep(channels);
-    }
-    // Filter down to channels that match our spec
-    var filteredChannels = channels.filter(lodash_1.matches(channelMatch));
-    // Iterate over the channels and filter down to the channel properties we want
-    filteredChannels.forEach(function (channel, index, array) {
-        array[index] = filterProps(channel, channelPropertyFilter);
-    });
-    return filteredChannels;
-}
-// Returns a filtered copy of a moderator list
-function FilterModerators(mods, filterOptions, modMatch, modPropFilter, channelMatch, channelPropFilter) {
-    // Apply filter options
-    if (filterOptions.mutation == "copy") {
-        mods = lodash_1.cloneDeep(mods);
-    }
-    var filteredMods = mods.filter(lodash_1.matches(modMatch));
-    filteredMods.forEach(function (mod, index, array) {
-        // Filter each mod's channels
-        mod.channels = FilterChannels(mod.channels, filterOptions, channelMatch, channelPropFilter);
-        // Filter down to the mod properties we want
-        array[index] = filterProps(mod, modPropFilter);
-    });
-    return filteredMods;
-}
 var partneredChannelMatch = { partnered: true };
 var partneredChannelPropertyFilter = ["name", "partnered"];
 var ModeratorPropertyFilter = ["user", "channels"];
-console.log("\nMods and Their Partnered Channels: \n" + (JSON.stringify(FilterModerators(SoManyMods, { mutation: "copy" }, null, ModeratorPropertyFilter, partneredChannelMatch, partneredChannelPropertyFilter), null, "\t")));
+console.log("\nMods and Their Partnered Channels: \n" + (JSON.stringify(NightBotModCheck_1.FilterModerators(SoManyMods, { mutation: "copy" }, null, ModeratorPropertyFilter, partneredChannelMatch, partneredChannelPropertyFilter), null, "\t")));
 var notPartneredChannelMatch = { partnered: false };
 var coolModsNamedWordsModMatch = { user: "woooords" };
-console.log("\nCooler Mods Named Words and Their Not-Partnered Channels: " + JSON.stringify(FilterModerators(SoManyMods, { mutation: "copy" }, coolModsNamedWordsModMatch, ModeratorPropertyFilter, notPartneredChannelMatch, partneredChannelPropertyFilter), null, "\t"));
-var wooordsMod = SoManyMods[0];
-var channelsModerated = "Wooords moderates for: ";
-wooordsMod.channels.forEach(function (channel) {
-    channelsModerated = channelsModerated + channel.name + ", ";
-});
-channelsModerated = channelsModerated.substring(0, channelsModerated.length - 2);
-console.log(channelsModerated);
+console.log("\nCooler Mods Named Words and Their Not-Partnered Channels: " + JSON.stringify(NightBotModCheck_1.FilterModerators(SoManyMods, { mutation: "copy" }, coolModsNamedWordsModMatch, ModeratorPropertyFilter, notPartneredChannelMatch, partneredChannelPropertyFilter), null, "\t"));
 //# sourceMappingURL=index.js.map
