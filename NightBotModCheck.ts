8afbf8c27c55497906495ad5efb0ec1125b0152c
Patrick Boyd <ppatboyd@outlook.com>
2021-05-22 17:22:52 -0700

    Move types and related functions to NightBotModCheck

---
+++ NightBotModCheck.ts
@@ -0,0 +1,72 @@
+import { cloneDeep, matches } from "lodash";
+
+export type Channel = {
+  name?: string,
+  followers?: number,
+  views?: number,
+  partnered?: boolean,
+};
+
+export type Moderator = {
+  status?: number,
+  user?: string,
+  cursor?: string,
+  channels?: Channel[],
+};
+
+export type FilterActionOptions = {
+  mutation: "copy" | "inplace";
+}
+
+export function filterProps<T>(channel: T, allowedProps: (keyof T)[]) : T {
+  return Object.fromEntries(
+    Object.entries(channel).filter(([key/*, value*/]) => {
+      return (allowedProps.includes(key as keyof T));
+    })
+  ) as T;
+}
+
+// Returns a filtered copy of a channel list
+export function FilterChannels(channels: Channel[], filterOptions : FilterActionOptions, channelMatch : Channel, channelPropertyFilter : (keyof Channel)[]) : Channel[] {
+
+  // Apply filter options
+  if (filterOptions.mutation == "copy")
+  {
+    channels = cloneDeep(channels);
+  }
+
+  // Filter down to channels that match our spec
+  const filteredChannels = channels.filter(matches(channelMatch));
+    
+  // Iterate over the channels and filter down to the channel properties we want
+  filteredChannels.forEach(
+    (channel: Channel, index: number, array : Channel[]) => {
+      array[index] = filterProps<Channel>(channel, channelPropertyFilter);
+    }
+  );
+
+  return filteredChannels;
+}
+
+// Returns a filtered copy of a moderator list
+export function FilterModerators(mods: Moderator[], filterOptions : FilterActionOptions, modMatch : Moderator, modPropFilter : (keyof Moderator)[], channelMatch : Channel, channelPropFilter : (keyof Channel)[]) : Moderator[] {
+
+  // Apply filter options
+  if (filterOptions.mutation == "copy")
+  {
+    mods = cloneDeep(mods);
+  }
+
+  const filteredMods : Moderator[] = mods.filter(matches(modMatch));
+
+  filteredMods.forEach((mod: Moderator, index : number, array : Moderator[]) => {
+
+    // Filter each mod's channels
+    mod.channels = FilterChannels(mod.channels, filterOptions, channelMatch, channelPropFilter);
+
+    // Filter down to the mod properties we want
+    array[index] = filterProps<Moderator>(mod, modPropFilter);
+  })
+
+  return filteredMods;
+}
--- index.ts
+++ index.ts
@@ -1,22 +1,4 @@
-import { cloneDeep, matches } from "lodash";
-
-type Channel = {
-  name?: string,
-  followers?: number,
-  views?: number,
-  partnered?: boolean,
-};
-
-type Moderator = {
-  status?: number,
-  user?: string,
-  cursor?: string,
-  channels?: Channel[],
-};
-
-type FilterActionOptions = {
-  mutation: "copy" | "inplace";
-}
+import { Channel, FilterModerators, Moderator } from "./NightBotModCheck";
 
 const SoManyMods: Moderator[] = [{
     status:200,
@@ -61,60 +43,6 @@ const SoManyMods: Moderator[] = [{
           }]
       }];
 
-
-  function filterProps<T>(channel: T, allowedProps: (keyof T)[]) : T {
-    return Object.fromEntries(
-      Object.entries(channel).filter(([key/*, value*/]) => {
-        return (allowedProps.includes(key as keyof T));
-      })
-    ) as T;
-  }
-
-  // Returns a filtered copy of a channel list
-  function FilterChannels(channels: Channel[], filterOptions : FilterActionOptions, channelMatch : Channel, channelPropertyFilter : (keyof Channel)[]) : Channel[] {
-
-    // Apply filter options
-    if (filterOptions.mutation == "copy")
-    {
-      channels = cloneDeep(channels);
-    }
-
-    // Filter down to channels that match our spec
-    const filteredChannels = channels.filter(matches(channelMatch));
-      
-    // Iterate over the channels and filter down to the channel properties we want
-    filteredChannels.forEach(
-      (channel: Channel, index: number, array : Channel[]) => {
-        array[index] = filterProps<Channel>(channel, channelPropertyFilter);
-      }
-    );
-
-    return filteredChannels;
-  }
-  
-  // Returns a filtered copy of a moderator list
-  function FilterModerators(mods: Moderator[], filterOptions : FilterActionOptions, modMatch : Moderator, modPropFilter : (keyof Moderator)[], channelMatch : Channel, channelPropFilter : (keyof Channel)[]) : Moderator[] {
-
-    // Apply filter options
-    if (filterOptions.mutation == "copy")
-    {
-      mods = cloneDeep(mods);
-    }
-
-    const filteredMods : Moderator[] = mods.filter(matches(modMatch));
-
-    filteredMods.forEach((mod: Moderator, index : number, array : Moderator[]) => {
-
-      // Filter each mod's channels
-      mod.channels = FilterChannels(mod.channels, filterOptions, channelMatch, channelPropFilter);
-
-      // Filter down to the mod properties we want
-      array[index] = filterProps<Moderator>(mod, modPropFilter);
-    })
-
-    return filteredMods;
-  }
-
   const partneredChannelMatch : Channel = {partnered: true};
   const partneredChannelPropertyFilter : (keyof Channel)[] = ["name", "partnered"];
   const ModeratorPropertyFilter : (keyof Moderator)[] = ["user", "channels"];
@@ -125,16 +53,3 @@ const SoManyMods: Moderator[] = [{
   const coolModsNamedWordsModMatch : Moderator = { user: "woooords" };
 
   console.log("\nCooler Mods Named Words and Their Not-Partnered Channels: " + JSON.stringify(FilterModerators(SoManyMods, { mutation: "copy" }, coolModsNamedWordsModMatch, ModeratorPropertyFilter, notPartneredChannelMatch, partneredChannelPropertyFilter), null, "\t"));
-
-
-  const wooordsMod = SoManyMods[0];
-
-  let channelsModerated : string = "Wooords moderates for: ";
-
-  wooordsMod.channels.forEach((channel:Channel) =>{
-    channelsModerated = channelsModerated + channel.name + ", ";
-  });
-
-  channelsModerated = channelsModerated.substring(0, channelsModerated.length-2);
-
-  console.log(channelsModerated);
